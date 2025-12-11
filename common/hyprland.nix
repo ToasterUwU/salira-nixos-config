06aad1e0d4541509d31fa727b14bc10ff17c6e58
SaliraDaxius <saliradaxius@proton.me>
2025-12-11 14:17:21 -0500

    More hyprland setup

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -3,11 +3,20 @@
     enable = true;
     withUWSM = true; # recommended for most users
     xwayland.enable = true; # Xwayland can be disabled.
+    nvidiaPatches = true;
   };
 
   # Desktop portal
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    # WLR_NO_HARDWARE_CURSORS = "1";
+
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
 
   environment.systemPackages = with pkgs; [
     # Default terminal, can be removed once config is set to preference
@@ -19,5 +28,10 @@
     # Notification daemon (one is required)
     dunst
     # mako # Users say this is dunst but with more options
+
+    # Taskbar
+    waybar
+
+    rofi-wayland
   ];
 }
