1987b851f98d026040063528f6e5ecb7d718daa8
SaliraDaxius <saliradaxius@proton.me>
2025-12-10 19:21:12 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations.theseus = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+      ];
+    };
+
+  };
+}
